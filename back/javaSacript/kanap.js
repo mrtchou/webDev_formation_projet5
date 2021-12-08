23b5b00f3ccf73cb41bb123b5eef10dd33a0fcb4
mrtchou <77937111+mrtchou@users.noreply.github.com>
2021-12-08 10:54:48 +0100

    Update kanap.js
    creation element "a" dans le dom, puis l'element "article", "img", "h3", "p"
    pour ensuite placer les data depuis api sur la page index.html avec la boucle "for in" qui est inclu dans une function

--- back/javaSacript/kanap.js
+++ back/javaSacript/kanap.js
@@ -17,11 +17,56 @@ async function sectionARemplir() {
         for (let article in articles) {
 
 
-
-            // Insertion de l'élément "a"
-            let productLink = document.createElement("a");
+ let productLink = document.createElement("a");
             document.querySelector(".items").appendChild(productLink);
             productLink.href = `product.html?id=${resultatAPI[article]._id}`;
+
+
+            let productArticle = document.createElement("article");
+            productLink.appendChild(productArticle);
+
+
+
+
+
+
+
+
+
+
+
+
+            let productImg = document.createElement("img");
+            productArticle.appendChild(productImg);
+            productImg.src = resultatAPI[article].imageUrl;
+            productImg.alt = resultatAPI[article].altTxt;
+
+
+
+
+
+
+
+
+
+            let productName = document.createElement("h3");
+            productArticle.appendChild(productName);
+            productName.classList.add("productName");
+            productName.innerHTML = resultatAPI[article].name;
+
+
+
+
+
+
+
+
+
+
+            let productDescription = document.createElement("p");
+            productArticle.appendChild(productDescription);
+            productDescription.classList.add("productName");
+            productDescription.innerHTML = resultatAPI[article].description;
 
         }
     })
