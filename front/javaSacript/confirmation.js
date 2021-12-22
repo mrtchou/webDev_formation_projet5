fddfa3318d8717b17bb6343bcfdd1da4294fbd43
mrtchou <77937111+mrtchou@users.noreply.github.com>
2021-12-22 22:33:48 +0100

    Update confirmation.js
    modification pour clear localStorage
    suppression uniquement de orderId

--- front/javaSacript/confirmation.js
+++ front/javaSacript/confirmation.js
@@ -1,15 +1,17 @@
 
 //avec la function  je selection balise id
 //puis depuis local storage jinsere au format txt les donne de local storage a l'endroit de cette balise pour afficher id order
-//puis a la fin je vide le local storage
+//puis a la fin je vide le local storage avec key "produit"
 
 function main() {
+
     const numeroCommande = document.getElementById("orderId");
 
     numeroCommande.textContent = localStorage.getItem("orderId");
+    console.log(localStorage.getItem("orderId"))
+    //localStorage.clear();
+    localStorage.removeItem("produit");
 
-
-    localStorage.clear();
 }
 
 main();
